38868a871ba5214e3022e6eac109eca989cda252
mithi5 <mithi5@gmail.com>
2022-08-05 07:25:54 +0000

    initial commit

---
+++ server/src/main/java/com/example/great.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Great {
+
+  /**
+   * This is a constructor.
+   */
+  public Great() {
+
+  }
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
